fcd961fcf23c46981d6767b619ee07f551cf73ec
Pavel K <102539606+think-outside-the-box@users.noreply.github.com>
2022-04-18 14:22:36 +0300

    connect via web driver
    Chrome Selenium Web Driver Test
    Login to db using Chrome Selenium Web Driver

---
+++ RedBull.java
@@ -0,0 +1,26 @@
+package SereniumApp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class RedBull {
+
+    public static  void  main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Pavel\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("http://bulletins-pu.thomasmurray.com/");
+        System.out.println(driver.getTitle());
+        driver.findElement(By.name("username")).sendKeys("test");
+        driver.findElement(By.name("password")).sendKeys("dkYttNb4Jc(%zTRu");
+        driver.findElement(By.className("button1")).click();
+        driver.quit();
+    }
+
+}
